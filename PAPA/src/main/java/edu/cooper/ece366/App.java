1612f706a81fbf0c3b5caf10888f4dd5274e8076
Allister Liu <allisterakun@gmail.com>
2021-02-07 11:01:59 -0800

    created project

---
+++ PAPA/src/main/java/edu/cooper/ece366/App.java
@@ -0,0 +1,13 @@
+package edu.cooper.ece366;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
