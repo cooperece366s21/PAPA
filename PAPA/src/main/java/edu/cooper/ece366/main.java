be63fe60cc60d00a393656509cb76206ef611848
Philip Blumin <philipblumin@philips-mbp.lan>
2021-02-10 16:25:22 -0500

    first draft of main.java

--- PAPA/src/main/java/edu/cooper/ece366/main.java
+++ PAPA/src/main/java/edu/cooper/ece366/main.java
@@ -7,3 +7,84 @@ plan for now:
 - they can enter l or r to simulate the swipe
 - when finishing all the options well return the options they liked
  */
+package edu.cooper.ece366;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class main
+{
+    public static void main( String[] args )
+    {
+        //category list
+        ArrayList<String> catList = new ArrayList<String>();
+        catList.add("Resturants");
+        catList.add("Movies (DO NOT SELECT FOR NOW!)");
+
+        //test resturant list
+        ArrayList<String> resturants = new ArrayList<String>();
+        resturants.add("Smac");
+        resturants.add("5 guys");
+        resturants.add("Artichoke");
+        resturants.add("2 Bros");
+        resturants.add("Mamouns");
+        resturants.add("Stickies");
+
+        System.out.println("Select option from one of the following categories: \n");
+
+        //display options
+        for(String categories:catList) {
+            System.out.println(categories + "   ");
+        }
+        System.out.println("\n");
+
+        Scanner selectedCat = new Scanner(System.in);  // Create a Scanner object
+
+        String userSelectedCat = selectedCat.nextLine();  // Read user input
+
+        //check to make sure a valid option was selected
+        while(!catList.contains(userSelectedCat)){
+            System.out.println("Please enter one of the above options: \n");
+
+            userSelectedCat = selectedCat.nextLine();
+        }
+
+        System.out.println("You have selected the following category: " + userSelectedCat + "\n");
+
+        //initialize the left and right swipe lists
+        ArrayList<String> rightList = new ArrayList<String>();
+        ArrayList<String> leftList = new ArrayList<String>();
+
+        Scanner swipe = new Scanner(System.in);
+        String userSwipe;
+        //change to work for any category selected
+        for(String option:resturants) {
+            System.out.println("Type in l (dislike) or r (like): ");
+            userSwipe = swipe.nextLine();
+
+            while(userSwipe != "l" || userSwipe != "r" ){
+                System.out.println("Please enter l or r \n");
+                userSwipe = swipe.nextLine();
+            }
+
+            if(userSwipe != "l"){
+                leftList.add(option);
+            } else {
+                rightList.add(option);
+            }
+        }
+
+        System.out.println("Here is the list of everything you liked: \n");
+        for(String liked:rightList) {
+            System.out.println(liked + "\n");
+        }
+
+        Random rand = new Random();
+        String randChoice = rightList.get(rand.nextInt(rightList.size()));
+
+        System.out.println("Our recommendation is to go to: " + randChoice);
+
+    }
+}
