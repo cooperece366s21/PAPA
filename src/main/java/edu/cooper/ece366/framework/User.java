1da0c17b68d2c12f6df00bbdd9b4421f17fd0908
Leong2 <leong2@seniortest2>
2021-02-27 20:19:26 -0500

    updated user

--- src/main/java/edu/cooper/ece366/framework/User.java
+++ src/main/java/edu/cooper/ece366/framework/User.java
@@ -12,28 +12,22 @@ import java.util.Random;
 public class User {                  //NOTE: CHANGE STRING TO TYPE RESTAURANT IN ARRAYLIST!!!!!
     private String ID;
 
-    public User(){            //initialize a user called "name" if no parameters specified
-        this.ID= "name";
-        this.leftList= new ArrayList<String>();
-        this.rightList= new ArrayList<String>();
+    public User(){
+        this.ID= generateID();
+        this.leftList= new ArrayList<Restaurant>();
+        this.rightList= new ArrayList<Restaurant>();
     }
 
-    public User(String username){
-        this.ID= username;
-        this.leftList= new ArrayList<String>();
-        this.rightList= new ArrayList<String>();
-    }
-
-    ArrayList<String> leftList=new ArrayList<String>();
-    ArrayList<String> rightList=new ArrayList<String>();
+    ArrayList<Restaurant> leftList=new ArrayList<Restaurant>();
+    ArrayList<Restaurant> rightList=new ArrayList<Restaurant>();
     //private Queue <String> restaurantQueue = new LinkedList<>();
-    ArrayList<String> restaurants = new ArrayList<String>();
+    ArrayList<Restaurant> restaurants = new ArrayList<Restaurant>();
 
-    public ArrayList<String> getRestaurants() {
+    public ArrayList<Restaurant> getRestaurants() {
         return restaurants;
     }
 
-    void output (ArrayList<String>restaurants) {
+    void output (ArrayList<Restaurant>restaurants) {
         Scanner swipe = new Scanner(System.in);
         String userSwipe;
 
@@ -82,6 +76,31 @@ public class User {                  //NOTE: CHANGE STRING TO TYPE RESTAURANT IN
         return uniqueID;
     }
 
+
+
+/*    public void selectCategory(catList) {
+        for(String categories:catList) {
+            System.out.println(categories + "   ");
+        }
+        System.out.println("\n");
+
+        Scanner selectedCat = new Scanner(System.in);  // Create a Scanner object
+
+        String userSelectedCat = selectedCat.nextLine();  // Read user input
+
+        //check to make sure a valid option was selected
+        while(!catList.contains(userSelectedCat)){
+            System.out.println("Please enter one of the above options: \n");
+
+            userSelectedCat = selectedCat.nextLine();
+        }
+
+        System.out.println("You have selected the following category: " + userSelectedCat + "\n");
+
+
+    }*/
+
+
     //Getters and setters
 
     public String getID() {
@@ -91,11 +110,11 @@ public class User {                  //NOTE: CHANGE STRING TO TYPE RESTAURANT IN
         this.ID = ID;
     }
 
-    public void setLeftList(ArrayList<String> leftList) {
+    public void setLeftList(ArrayList<Restaurant> leftList) {
         this.leftList = leftList;
     }
 
-    public void setRightList(ArrayList<String> rightList) {
+    public void setRightList(ArrayList<Restaurant> rightList) {
         this.rightList = rightList;
     }
 }
