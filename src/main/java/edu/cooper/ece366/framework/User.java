5dff0cb3a754f09616101af405e3e32e8ff9675a
Leong2 <leong2@seniortest2>
2021-03-06 17:16:59 -0500

    modified user

--- src/main/java/edu/cooper/ece366/framework/User.java
+++ src/main/java/edu/cooper/ece366/framework/User.java
@@ -6,114 +6,75 @@ import java.util.Random;
 import java.util.Scanner;
 import java.util.UUID;
 
-public class User {                  //NOTE: CHANGE STRING TO TYPE RESTAURANT IN ARRAYLIST!!!!!
+public class User {
     private UUID ID;
 
     public User(){
         this.ID= generateID();
-        this.leftList= new ArrayList<Restaurant>();
-        this.rightList= new ArrayList<Restaurant>();
+//        this.leftList= new ArrayList<Restaurant>();
+//        this.rightList= new ArrayList<Restaurant>();
     }
+//
+//    ArrayList<Restaurant> leftList=new ArrayList<Restaurant>();
+//    ArrayList<Restaurant> rightList=new ArrayList<Restaurant>();
+//    ArrayList<Restaurant> restaurants = new ArrayList<Restaurant>();
+//
+//    public ArrayList<Restaurant> getRestaurants() {
+//        return restaurants;
+//    }
 
-    ArrayList<Restaurant> leftList=new ArrayList<Restaurant>();
-    ArrayList<Restaurant> rightList=new ArrayList<Restaurant>();
-    //private Queue <String> restaurantQueue = new LinkedList<>();
-    ArrayList<Restaurant> restaurants = new ArrayList<Restaurant>();
-
-    public ArrayList<Restaurant> getRestaurants() {
-        return restaurants;
-    }
-
-    public void output(ArrayList<Restaurant> restaurants) {
-        // some of this code could be used for demo, but you'll want this separate from the server as a mock "client"
-        Scanner swipe = new Scanner(System.in);
-        String userSwipe;
-
-        for(Restaurant option:restaurants) {
-            System.out.println(option.toString() + "\n");
-            System.out.println("Type in l (dislike) or r (like): ");
-            userSwipe = swipe.nextLine();
-
-            while(!userSwipe.equals("l") && !userSwipe.equals("r")){
-                System.out.println("Please enter l or r \n");
-                userSwipe = swipe.nextLine();
-            }
-
-            //add to the left and right lists
-            if(userSwipe.equals("l")){
-                leftList.add(option);
-            } else {
-                rightList.add(option);
-            }
-
-        }
-
-        if(rightList.isEmpty()){
-
-            System.out.println("You did not like any of the options :(\n");
-
-        } else {
-
-            System.out.println("Here is the list of everything you liked: \n");
-            for (Restaurant liked : rightList) {
-                System.out.println(liked.getName() + "\n");
-            }
-
-            //randomly pick from the options you liked
-            Random rand = new Random();
-            Restaurant randChoice = rightList.get(rand.nextInt(rightList.size()));
-
-            System.out.println("Our recommendation is to go to: " + randChoice.toString());
-
-        }
-
-    }
+//    public void output(ArrayList<Restaurant> restaurants) {
+//        // some of this code could be used for demo, but you'll want this separate from the server as a mock "client"
+//        Scanner swipe = new Scanner(System.in);
+//        String userSwipe;
+//
+//        for(Restaurant option:restaurants) {
+//            System.out.println(option.toString() + "\n");
+//            System.out.println("Type in l (dislike) or r (like): ");
+//            userSwipe = swipe.nextLine();
+//
+//            while(!userSwipe.equals("l") && !userSwipe.equals("r")){
+//                System.out.println("Please enter l or r \n");
+//                userSwipe = swipe.nextLine();
+//            }
+//
+//            //add to the left and right lists
+//            if(userSwipe.equals("l")){
+//                leftList.add(option);
+//            } else {
+//                rightList.add(option);
+//            }
+//
+//        }
+//
+//        if(rightList.isEmpty()){
+//
+//            System.out.println("You did not like any of the options :(\n");
+//
+//        } else {
+//
+//            System.out.println("Here is the list of everything you liked: \n");
+//            for (Restaurant liked : rightList) {
+//                System.out.println(liked.getName() + "\n");
+//            }
+//
+//            //randomly pick from the options you liked
+//            Random rand = new Random();
+//            Restaurant randChoice = rightList.get(rand.nextInt(rightList.size()));
+//
+//            System.out.println("Our recommendation is to go to: " + randChoice.toString());
+//
+//        }
+//
+//    }
 
     public UUID generateID(){
-        UUID uniqueID= UUID.randomUUID();
-        return uniqueID;
+        return UUID.randomUUID();
     }
-
-
-
-/*    public void selectCategory(catList) {
-        for(String categories:catList) {
-            System.out.println(categories + "   ");
-        }
-        System.out.println("\n");
-
-        Scanner selectedCat = new Scanner(System.in);  // Create a Scanner object
-
-        String userSelectedCat = selectedCat.nextLine();  // Read user input
-
-        //check to make sure a valid option was selected
-        while(!catList.contains(userSelectedCat)){
-            System.out.println("Please enter one of the above options: \n");
-
-            userSelectedCat = selectedCat.nextLine();
-        }
-
-        System.out.println("You have selected the following category: " + userSelectedCat + "\n");
-
-
-    }*/
-
-
-    //Getters and setters
 
     public UUID getID() {
         return ID;
     }
-    public void setID(UUID ID) {
-        this.ID = ID;
-    }
 
-    public void setLeftList(ArrayList<Restaurant> leftList) {
-        this.leftList = leftList;
-    }
-
-    public void setRightList(ArrayList<Restaurant> rightList) {
-        this.rightList = rightList;
-    }
 }
 
