a469d1768b22c070ae5dc259add334520e9b9911
pcucch <pmcuc4@gmail.com>
2021-03-06 18:59:08 -0500

    lobby updates

--- src/main/java/edu/cooper/ece366/store/LobbyStore.java
+++ src/main/java/edu/cooper/ece366/store/LobbyStore.java
@@ -9,5 +9,13 @@ import java.util.UUID;
 
 public interface LobbyStore {
     Lobby get(UUID id);
+    String getCode(Lobby lobby)
+
+    // Function to get the list of Restaurants within a certain amount of miles
+    List<Restaurant> getByLocation(Double miles);
+
+//    List<Content> getByRating(Rating rating);
+
+    void beginSearch(List<Users> users, List<Restaurants> resteraunts);
 
 }
--- src/main/java/edu/cooper/ece366/store/LobbyStoreImpl.java
+++ src/main/java/edu/cooper/ece366/store/LobbyStoreImpl.java
@@ -11,4 +11,36 @@ public class LobbyStoreImpl implements LobbyStore {
     public Lobby get(UUID id) {
         return null;
     }
+
+    @Override
+    public List<Restauraunt> getByLocation(final double miles) {
+        return null;
+//                contentMap.values().stream()
+//                .filter(content -> genre.equals(content.genre()))
+//                .collect(Collectors.toList());
+    }
+
+// Put the hard coded info in here
+
+//    static {
+//        List<Content> content =
+//                List.of(
+//                        new MovieBuilder().genre(Genre.COMEDY).title("Ferris Bueller").rating(Rating.PG13).id("1").build(),
+//                        new MovieBuilder().genre(Genre.COMEDY).title("My Cousin Vinny").id("2").build(),
+//                        new MovieBuilder().genre(Genre.COMEDY).title("Pink Panther").id("3").build(),
+//                        new MovieBuilder().genre(Genre.HORROR).title("Scream").id("4").build(),
+//                        new MovieBuilder().genre(Genre.HORROR).title("Scream 2").id("5").build(),
+//                        new MovieBuilder().genre(Genre.HORROR).title("Scream 3").id("6").build(),
+//                        new MovieBuilder().genre(Genre.ACTION).title("Mission Impossible").id("7").build(),
+//                        new MovieBuilder().genre(Genre.ACTION).title("007").id("8").build(),
+//                        new MovieBuilder().genre(Genre.ACTION).title("Mad Max: Fury Road").id("9").build(),
+//                        new MovieBuilder()
+//                                .genre(Genre.MYSTERY)
+//                                .title("Scooby Doo: Escape from Island")
+//                                .id("10")
+//                                .build(),
+//                        new MovieBuilder().genre(Genre.MYSTERY).title("Wrath of Kahn").id("11").build(),
+//                        new MovieBuilder().genre(Genre.MYSTERY).title("Murder Mystery").id("12").build());
+//        contentMap = content.stream().collect(Collectors.toMap(Content::id, Function.identity()));
+//    }
 }
