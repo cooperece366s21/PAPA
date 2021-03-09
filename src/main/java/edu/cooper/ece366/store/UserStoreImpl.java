0999df2a271ca3f86ea283b20993fc92af2a3504
Philip Blumin <philipblumin@philips-mbp.lan>
2021-03-08 20:32:59 -0500

    hard coded like/dislike lists for now

--- src/main/java/edu/cooper/ece366/store/UserStoreImpl.java
+++ src/main/java/edu/cooper/ece366/store/UserStoreImpl.java
@@ -3,10 +3,13 @@ package edu.cooper.ece366.store;
 
 import edu.cooper.ece366.framework.User;
 import edu.cooper.ece366.framework.UserBuilder;
+import edu.cooper.ece366.store.RestaurantStoreImpl;
+
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+
 
 public class UserStoreImpl implements UserStore {
 
@@ -18,14 +21,23 @@ public class UserStoreImpl implements UserStore {
                         new UserBuilder()
                                 .ID("KollKid")
                                 .nickname("KollKid")
+                                .addLike(RestaurantStoreImpl.RESTAURANT_MAP.get("smac's-american-new-york"))
+                                .addLike(RestaurantStoreImpl.RESTAURANT_MAP.get("mamoun's-halal-new-york"))
+                                .addDisLike(RestaurantStoreImpl.RESTAURANT_MAP.get("panya-bakery-new-york"))
                                 .build(),
                         new UserBuilder()
                                 .ID("Pablo")
                                 .nickname("Pablo")
+                                .addLike(RestaurantStoreImpl.RESTAURANT_MAP.get("smac's-american-new-york"))
+                                .addLike(RestaurantStoreImpl.RESTAURANT_MAP.get("mamoun's-halal-new-york"))
+                                .addLike(RestaurantStoreImpl.RESTAURANT_MAP.get("panya-bakery-new-york"))
                                 .build(),
                         new UserBuilder()
                                 .ID("xXx_Sephiroth_xXx")
                                 .nickname("xXx_Sephiroth_xXx")
+                                .addLike(RestaurantStoreImpl.RESTAURANT_MAP.get("smac's-american-new-york"))
+                                .addDisLike(RestaurantStoreImpl.RESTAURANT_MAP.get("mamoun's-halal-new-york"))
+                                .addDisLike(RestaurantStoreImpl.RESTAURANT_MAP.get("panya-bakery-new-york"))
                                 .build()
                 ).stream()
                 .collect(Collectors.toMap(User::ID, Function.identity()));
