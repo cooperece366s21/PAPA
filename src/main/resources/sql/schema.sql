c8b57a52b3f6278de9066383082a9e0352da84fd
pcucch <pmcuc4@gmail.com>
2021-03-27 18:24:48 -0400

    databse start

---
+++ src/main/resources/sql/schema.sql
@@ -0,0 +1,36 @@
+create table if not exists users
+(
+    id       varchar(256) primary key not null,
+    name     varchar(36) not null
+    );
+
+create table if not exists lobbies
+(
+    id       varchar(256) primary key not null,
+    code     varchar(36) not null
+    # Come ack and put in list of users in lobby and restaurants in lobby
+    );
+
+create table if not exists restaurants
+(
+    id       varchar(256) primary key not null,
+    name     varchar(36) not null
+    );
+
+create table if not exists user_preferred_restaurants
+(
+    user_id varchar(36) not null,
+    rest_id varchar(36) not null,
+    preference enum('dislike', 'like'),
+    primary key (user_id, rest_id),
+    foreign key (user_id) references users (id)
+    );
+
+create table if not exists lobby_preferred_restaurants
+(
+    lobby_id varchar(36) not null,
+    rest_id varchar(36) not null,
+    votes varchar(36) not null,
+    primary key (lobby_id, rest_id),
+    foreign key (lobby_id) references users (id)
+    );
