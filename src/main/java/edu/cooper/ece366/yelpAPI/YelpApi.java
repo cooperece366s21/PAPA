1103bab6c6b5b56c6721a208d8c9ea976e253502
Allister Liu <allisterakun@gmail.com>
2021-03-31 17:56:24 -0700

    added function descriptions and usages for the three search methods

--- src/main/java/edu/cooper/ece366/yelpAPI/YelpApi.java
+++ src/main/java/edu/cooper/ece366/yelpAPI/YelpApi.java
@@ -27,7 +27,22 @@ public class YelpApi {
         this.APIkey = Files.readString(Path.of("yelpAPIkey.txt"));
     }
 
-
+    /**
+     * function to search for n=limit restaurants within a given location, with a optional keyword = (String) term
+     * @param term Optional search keyword:
+     *      *      - can be search term (i.e. "food" or "restaurant")
+     *      *      - can be business name (i.e. "Starbucks")
+     *      *      - If term is not included the endpoint will default to searching across businesses from
+     *      *        a small number of popular categories.
+     * @param location This string indicates the geographic area to be used when searching for businesses:
+     *      *          - can be country name, state name, city name, street name... (i.e. "NYC")
+     *      *          - can be a region name (i.e. "east village")
+     *      *          - can be ZIP code (i.e. "10003")
+     *      *          - can be a precise address (i.e. "41 Cooper Sq, New York, NY 10003")
+     * @param limit Optional. Number of business results to return. By default, it will return 20. Maximum is 50.
+     * @return JSONArray of returned business objects.
+     *      *    Response body format: https://www.yelp.com/developers/documentation/v3/business_search
+     */
     public JSONArray searchForBusinessesByLocation(@Nullable String term, String location, @Nullable Integer limit) {
         String searchTerm = term == null ? "" : term;
         int searchLimit = limit == null ? SEARCH_LIMIT : limit;
@@ -41,6 +56,19 @@ public class YelpApi {
         return response.getBody().getObject().getJSONArray("businesses");
     }
 
+    /**
+     *
+     * @param term optional search keyword:
+     *      *             - can be search term (i.e. "food" or "restaurant")
+     *      *             - can be business name (i.e. "Starbucks")
+     *      *             - If term is not included the endpoint will default to searching across businesses from
+     *      *               a small number of popular categories.
+     * @param lat Latitude of the location you want to search nearby.
+     * @param lng Longitude of the location you want to search nearby.
+     * @param limit Optional. Number of business results to return. By default, it will return 20. Maximum is 50.
+     * @return JSONArray of returned business objects.
+     *      *    Response body format: https://www.yelp.com/developers/documentation/v3/business_search
+     */
     public JSONArray searchForBusinessesByGeo(@Nullable String term, double lat, double lng, @Nullable Integer limit) {
         String searchTerm = term == null ? "" : term;
         int searchLimit = limit == null ? SEARCH_LIMIT : limit;
@@ -55,7 +83,12 @@ public class YelpApi {
 
     }
 
-
+    /**
+     *
+     * @param businessID Unique Yelp ID of business used to retrieve it detail information
+     * @return detailed business object.
+     *      *   Response body format: https://www.yelp.com/developers/documentation/v3/business
+     */
     public JSONObject searchByBusinessId(String businessID) {
         HttpResponse<JsonNode> response = Unirest.get(API_HOST + BUSINESS_PATH +"/" + businessID)
                 .header("Authorization", "Bearer " + APIkey)
